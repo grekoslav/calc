cdd7b3f8ccec04ccf67534ec4e0ed3a5d995af02
grekoslav <71317138+grekoslav@users.noreply.github.com>
2024-01-06 22:23:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ function backspace () {
 
 function equal () {
     let temp = document.form.textview.value;
-    document.form.textview.value = temp.slice(0, -1)
     if (temp) {
         document.form.textview.value = eval(temp)
     }
